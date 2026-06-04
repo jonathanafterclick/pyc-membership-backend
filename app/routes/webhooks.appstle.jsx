0b750a5b0e1fd858b7ea49c6b2e6599305233bce
jonathancrinquand <jonathancrinquand.jc@gmail.com>
2026-06-04 15:14:47 -0400

    Add Appstle webhook loader

--- app/routes/webhooks.appstle.jsx
+++ app/routes/webhooks.appstle.jsx
@@ -1,3 +1,9 @@
+export async function loader() {
+    return new Response("Appstle webhook endpoint is live", {
+        status: 200,
+    });
+}
+
 export async function action({ request }) {
     console.log("APPSTLE WEBHOOK RECEIVED");
 
